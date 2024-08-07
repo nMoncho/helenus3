479abbf51aacc451a421cff66b8e61edd83dd160
Gustavo De Micheli <gustavo.de.micheli@lunatech.com>
2024-08-07 14:56:55 +0200

    feat: Add CQL String Interpolation (async support)

--- core/src/main/scala/net/nmoncho/helenus/internal/macros/CqlQueryInterpolation.scala
+++ core/src/main/scala/net/nmoncho/helenus/internal/macros/CqlQueryInterpolation.scala
@@ -25,11 +25,14 @@ import java.nio.ByteBuffer
 
 import javax.xml.namespace.QName
 
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
 import scala.quoted.*
 
 import com.datastax.oss.driver.api.core.CqlSession
 import com.datastax.oss.driver.api.core.ProtocolVersion
 import com.datastax.oss.driver.api.core.cql.BoundStatement
+import com.datastax.oss.driver.api.core.cql.PreparedStatement
 import com.datastax.oss.driver.api.core.cql.Row
 import com.datastax.oss.driver.internal.core.util.Strings
 import net.nmoncho.helenus.api.DefaultColumnNamingScheme.map
@@ -39,9 +42,49 @@ import net.nmoncho.helenus.api.cql.WrappedBoundStatement
 
 object CqlQueryInterpolation:
 
-    def cqlImpl(parts: Expr[Seq[String]], params: Expr[Seq[Any]], session: Expr[CqlSession])(using
+    def cqlImpl(sc: Expr[StringContext], params: Expr[Seq[Any]], session: Expr[CqlSession])(using
     qctx: Quotes): Expr[WrappedBoundStatement[Row]] =
         import qctx.reflect.*
+
+        val (parts, terms) = partsAndTerms(sc, params)
+        val stmt           = Expr(buildStatement(parts, terms))
+        val bstmt          = '{ $session.prepare($stmt).bind() }
+        val estmt          = encode(bstmt, terms)
+
+        '{
+            new WrappedBoundStatement($estmt)(using RowMapper.identity)
+        }
+    end cqlImpl
+
+    def cqlAsyncImpl(
+        sc: Expr[StringContext],
+        params: Expr[Seq[Any]],
+        session: Expr[CqlSession],
+        ec: Expr[ExecutionContext]
+    )(using qctx: Quotes): Expr[Future[WrappedBoundStatement[Row]]] =
+        import qctx.reflect.*
+
+        val (parts, terms) = partsAndTerms(sc, params)
+        val stmt           = Expr(buildStatement(parts, terms))
+        val encodeStmt     = encodeLambda(terms)
+
+        '{
+            scala.jdk.javaapi.FutureConverters.asScala($session.prepareAsync($stmt)).map { pstmt =>
+                new WrappedBoundStatement($encodeStmt(pstmt.bind()))(using RowMapper.identity)
+            }($ec)
+        }
+    end cqlAsyncImpl
+
+    inline def partsAndTerms(sc: Expr[StringContext], params: Expr[Seq[Any]])(using
+    q: Quotes): (Seq[String], List[q.reflect.Term]) =
+        import q.reflect.*
+
+        val parts = sc match
+            case '{ StringContext(${ Varargs(rawParts) }*) } =>
+                Expr.ofSeq(rawParts).valueOrAbort
+
+            case _ =>
+                report.errorAndAbort("Invalid use of CQL String Interpolation")
 
         val terms = params.asTerm match
             case Inlined(_, _, Typed(Repeated(terms, _), _)) =>
@@ -52,12 +95,8 @@ object CqlQueryInterpolation:
                   "cql interpolation was expecting varargs as its input parameter, but got something different"
                 )
 
-        val stmt = encode(buildStatement(parts, terms, session), terms)
-
-        '{
-            new WrappedBoundStatement($stmt)(using RowMapper.identity)
-        }
-    end cqlImpl
+        parts -> terms
+    end partsAndTerms
 
     inline def namedParameter(using q: Quotes)(term: q.reflect.Term): String =
         val symbol = term.symbol.name.toString
@@ -72,14 +111,12 @@ object CqlQueryInterpolation:
       *
       * @param parts String Interpolated parts
       * @param terms String Interpolated parameters
-      * @param session Cassandra Session
       * @return CQL [[BoundStatement]] without any parameter bound
       */
-    inline def buildStatement(using
-    q: Quotes)(parts: Expr[Seq[String]], terms: List[q.reflect.Term], session: Expr[CqlSession]): Expr[BoundStatement] =
+    inline def buildStatement(using q: Quotes)(parts: Seq[String], terms: List[q.reflect.Term]): String =
         import q.reflect.*
 
-        val paramTokens: Expr[List[String]] = Expr {
+        val paramTokens: List[String] =
             terms.map: term =>
                 term.tpe.widenTermRefByName match
                     case ConstantType(c) =>
@@ -87,18 +124,13 @@ object CqlQueryInterpolation:
 
                     case _ =>
                         namedParameter(term)
-        }
 
-        '{
-            $session.prepare(
-              $parts
-                  .zip($paramTokens)
-                  .foldLeft(new StringBuilder()):
-                      case (acc, (part, param)) => acc.append(part).append(param)
-                  .append($parts.lastOption.getOrElse(""))
-                  .toString()
-            ).bind()
-        }
+        parts
+            .zip(paramTokens)
+            .foldLeft(new StringBuilder()):
+                case (acc, (part, param)) => acc.append(part).append(param)
+            .append(parts.lastOption.getOrElse(""))
+            .toString()
     end buildStatement
 
     /** Sets/Encodes all bind parameter into a [[BoundStatement]]
@@ -120,7 +152,7 @@ object CqlQueryInterpolation:
                         case '[t] => enc[t](bstmt, term.asExprOf[t])
     end encode
 
-    /** Sets/Encodes a Bind Parameter into a CQL BoundStatement
+    /** Sets/Encodes a single Bind Parameter into a CQL BoundStatement by searching its [[Codec]]
       *
       * @param bstmt BoundStatement to set the parameter into
       * @param expr parameter to set
@@ -143,5 +175,24 @@ object CqlQueryInterpolation:
                 report.errorAndAbort(f.explanation)
         end match
     end enc
+
+    /** Creates a lambda that given a [[BoundStatement]], it will set/encode all bind parameters
+      *
+      * @param terms bind parameters as Terms
+      * @return lambda expression
+      */
+    def encodeLambda(using q: Quotes)(terms: List[q.reflect.Term]): Expr[BoundStatement => BoundStatement] =
+        import q.reflect.*
+
+        val btpe = TypeRepr.of[BoundStatement]
+        val tpe  = MethodType(List("bstmt"))(_ => List(btpe), _ => btpe)
+        Lambda(
+          owner = Symbol.spliceOwner,
+          tpe   = tpe,
+          rhsFn = (sym, params) =>
+              val bstmt = params.head.asExprOf[BoundStatement]
+              encode(bstmt, terms).asTerm.changeOwner(sym)
+        ).asExprOf[BoundStatement => BoundStatement]
+    end encodeLambda
 
 end CqlQueryInterpolation
