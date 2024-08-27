ac139ec08d902b286e9dc11b4d9e778943d6ab9c
Gustavo De Micheli <gustavo.demicheli@gmail.com>
2024-08-27 13:46:21 +0200

    chore: Add tests on CqlSession extension methods for registering codecs

--- core/src/test/scala/net/nmoncho/helenus/PackageSpec.scala
+++ core/src/test/scala/net/nmoncho/helenus/PackageSpec.scala
@@ -21,6 +21,14 @@
 
 package net.nmoncho.helenus
 
+import scala.util.Success
+
+import com.datastax.oss.driver.internal.core.`type`.DefaultListType
+import com.datastax.oss.driver.internal.core.`type`.PrimitiveType
+import com.datastax.oss.protocol.internal.ProtocolConstants
+import net.nmoncho.helenus.api.`type`.codec.Codec
+import net.nmoncho.helenus.internal.codec.udt.IdenticalUDTCodec
+import net.nmoncho.helenus.models.Address
 import net.nmoncho.helenus.utils.CassandraSpec
 import org.scalatest.OptionValues.*
 import org.scalatest.matchers.should.Matchers
@@ -48,6 +56,30 @@ class PackageSpec extends AnyWordSpec with Matchers with CassandraSpec:
             session.executionProfile("another-profile") should not be defined
         }
 
+        "register codecs" in {
+            val listStringCodec = Codec[List[String]]
+
+            session.registerCodecs(listStringCodec) shouldBe a[Success[?]]
+
+            session.getContext.getCodecRegistry
+                .codecFor(
+                  new DefaultListType(new PrimitiveType(ProtocolConstants.DataType.VARCHAR), true)
+                ) shouldBe listStringCodec
+        }
+
+        "register UDT codecs" in {
+            val keyspace = session.sessionKeyspace.map(_.getName.asInternal()).getOrElse("")
+
+            withClue("registering codec with a keyspace") {
+                session.registerCodecs(
+                  IdenticalUDTCodec.derive[Address](keyspace = keyspace, name = "address")
+                ) shouldBe a[Success[?]]
+            }
+
+            withClue("registering codec without a specific keyspace") {
+                session.registerCodecs(IdenticalUDTCodec.derived[Address]) shouldBe a[Success[?]]
+            }
+        }
     }
 
 end PackageSpec
