52062c4dd175169cd3a3ba062afca325c9e1e576
Gustavo De Micheli <gustavo.demicheli@gmail.com>
2024-08-27 13:46:20 +0200

    feat: Add Sync ScalaPreparedStatement and implementations

--- core/src/main/scala/net/nmoncho/helenus/api/cql/ScalaPreparedStatement.scala
+++ core/src/main/scala/net/nmoncho/helenus/api/cql/ScalaPreparedStatement.scala
@@ -132,20 +132,198 @@ object ScalaPreparedStatement:
 
     private val log = LoggerFactory.getLogger(classOf[ScalaPreparedStatement[?, ?]])
 
-    case class CQLQuery(query: String, session: CqlSession)
+    case class CQLQuery(query: String, session: CqlSession) extends SyncCQLQuery
 
-    // trait SyncCQLQuery:
-    //     def query: String
-    //     def session: CqlSession
+    trait SyncCQLQuery:
+        def query: String
+        def session: CqlSession
+
+        // format: off
+
+        /** Prepares a query without parameters
+          *
+          * @return BoundStatement that can be called like a function
+          */
+        def prepareUnit: ScalaPreparedStatementUnit[Row] =
+            new ScalaPreparedStatementUnit[Row](session.prepare(query), RowMapper.identity, StatementOptions.default)
+
+        /** Prepares a query that will take 1 query parameter, which can be invoked like:
+          * {{{
+          *   import net.nmoncho.helenus._
+          *
+          *   val pstmt = "SELECT * FROM users WHERE id = ?".toCQL.prepare[String]
+          *   val bstmt = pstmt("bob")
+          * }}}
+          *
+          * @return BoundStatement that can be called like a function
+          */
+        def prepare[T1](implicit t1: Codec[T1]): ScalaPreparedStatement1[T1, Row] =
+            new ScalaPreparedStatement1[T1, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1)
+
+        /** Prepares a query that will take 2 query parameter, which can be invoked like:
+        * {{{
+        *   import net.nmoncho.helenus._
+        *
+        *   val pstmt = "SELECT * FROM users WHERE id = ? and age = ?".toCQL.prepare[String]
+        *   val bstmt = pstmt("bob", 42)
+        * }}}
+        *
+        * @return BoundStatement that can be called like a function
+        */
+        def prepare[T1, T2](implicit t1: Codec[T1], t2: Codec[T2]): ScalaPreparedStatement2[T1, T2, Row] =
+            new ScalaPreparedStatement2[T1, T2, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2)
+
+        /** Prepares a query that will take 3 query parameter
+         *
+         * @return BoundStatement that can be called like a function
+         */
+        def prepare[T1, T2, T3](implicit t1: Codec[T1], t2: Codec[T2], t3: Codec[T3]): ScalaPreparedStatement3[T1, T2, T3, Row] =
+            new ScalaPreparedStatement3[T1, T2, T3, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2, t3)
+
+        /** Prepares a query that will take 4 query parameter
+         *
+         * @return BoundStatement that can be called like a function
+         */
+        def prepare[T1, T2, T3, T4](implicit t1: Codec[T1], t2: Codec[T2], t3: Codec[T3], t4: Codec[T4]): ScalaPreparedStatement4[T1, T2, T3, T4, Row] =
+            new ScalaPreparedStatement4[T1, T2, T3, T4, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2, t3, t4)
+
+        /** Prepares a query that will take 5 query parameter
+         *
+         * @return BoundStatement that can be called like a function
+         */
+        def prepare[T1, T2, T3, T4, T5](implicit t1: Codec[T1], t2: Codec[T2], t3: Codec[T3], t4: Codec[T4], t5: Codec[T5]): ScalaPreparedStatement5[T1, T2, T3, T4, T5, Row] =
+            new ScalaPreparedStatement5[T1, T2, T3, T4, T5, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2, t3, t4, t5)
+
+        /** Prepares a query that will take 6 query parameter
+         *
+         * @return BoundStatement that can be called like a function
+         */
+        def prepare[T1, T2, T3, T4, T5, T6](implicit t1: Codec[T1], t2: Codec[T2], t3: Codec[T3], t4: Codec[T4], t5: Codec[T5], t6: Codec[T6]): ScalaPreparedStatement6[T1, T2, T3, T4, T5, T6, Row] =
+            new ScalaPreparedStatement6[T1, T2, T3, T4, T5, T6, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2, t3, t4, t5, t6)
+
+        /** Prepares a query that will take 7 query parameter
+         *
+         * @return BoundStatement that can be called like a function
+         */
+        def prepare[T1, T2, T3, T4, T5, T6, T7](implicit t1: TypeCodec[T1], t2: TypeCodec[T2], t3: TypeCodec[T3], t4: TypeCodec[T4], t5: TypeCodec[T5], t6: TypeCodec[T6], t7: TypeCodec[T7]): ScalaPreparedStatement7[T1, T2, T3, T4, T5, T6, T7, Row] =
+            new ScalaPreparedStatement7[T1, T2, T3, T4, T5, T6, T7, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2, t3, t4, t5, t6, t7)
+
+        /** Prepares a query that will take 8 query parameter
+         *
+         * @return BoundStatement that can be called like a function
+         */
+        def prepare[T1, T2, T3, T4, T5, T6, T7, T8](implicit t1: TypeCodec[T1], t2: TypeCodec[T2], t3: TypeCodec[T3], t4: TypeCodec[T4], t5: TypeCodec[T5], t6: TypeCodec[T6], t7: TypeCodec[T7], t8: TypeCodec[T8]): ScalaPreparedStatement8[T1, T2, T3, T4, T5, T6, T7, T8, Row] =
+            new ScalaPreparedStatement8[T1, T2, T3, T4, T5, T6, T7, T8, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2, t3, t4, t5, t6, t7, t8)
+
+        /** Prepares a query that will take 9 query parameter
+         *
+         * @return BoundStatement that can be called like a function
+         */
+        def prepare[T1, T2, T3, T4, T5, T6, T7, T8, T9](implicit t1: TypeCodec[T1], t2: TypeCodec[T2], t3: TypeCodec[T3], t4: TypeCodec[T4], t5: TypeCodec[T5], t6: TypeCodec[T6], t7: TypeCodec[T7], t8: TypeCodec[T8], t9: TypeCodec[T9]): ScalaPreparedStatement9[T1, T2, T3, T4, T5, T6, T7, T8, T9, Row] =
+            new ScalaPreparedStatement9[T1, T2, T3, T4, T5, T6, T7, T8, T9, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2, t3, t4, t5, t6, t7, t8, t9)
+
+        /** Prepares a query that will take 10 query parameter
+         *
+         * @return BoundStatement that can be called like a function
+         */
+        def prepare[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10](implicit t1: TypeCodec[T1], t2: TypeCodec[T2], t3: TypeCodec[T3], t4: TypeCodec[T4], t5: TypeCodec[T5], t6: TypeCodec[T6], t7: TypeCodec[T7], t8: TypeCodec[T8], t9: TypeCodec[T9], t10: TypeCodec[T10]): ScalaPreparedStatement10[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, Row] =
+            new ScalaPreparedStatement10[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10)
+
+        /** Prepares a query that will take 11 query parameter
+         *
+         * @return BoundStatement that can be called like a function
+         */
+        def prepare[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11](implicit t1: TypeCodec[T1], t2: TypeCodec[T2], t3: TypeCodec[T3], t4: TypeCodec[T4], t5: TypeCodec[T5], t6: TypeCodec[T6], t7: TypeCodec[T7], t8: TypeCodec[T8], t9: TypeCodec[T9], t10: TypeCodec[T10], t11: TypeCodec[T11]): ScalaPreparedStatement11[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, Row] =
+            new ScalaPreparedStatement11[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11)
+
+        /** Prepares a query that will take 12 query parameter
+         *
+         * @return BoundStatement that can be called like a function
+         */
+        def prepare[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12](implicit t1: TypeCodec[T1], t2: TypeCodec[T2], t3: TypeCodec[T3], t4: TypeCodec[T4], t5: TypeCodec[T5], t6: TypeCodec[T6], t7: TypeCodec[T7], t8: TypeCodec[T8], t9: TypeCodec[T9], t10: TypeCodec[T10], t11: TypeCodec[T11], t12: TypeCodec[T12]): ScalaPreparedStatement12[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, Row] =
+            new ScalaPreparedStatement12[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12)
+
+        /** Prepares a query that will take 13 query parameter
+         *
+         * @return BoundStatement that can be called like a function
+         */
+        def prepare[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13](implicit t1: TypeCodec[T1], t2: TypeCodec[T2], t3: TypeCodec[T3], t4: TypeCodec[T4], t5: TypeCodec[T5], t6: TypeCodec[T6], t7: TypeCodec[T7], t8: TypeCodec[T8], t9: TypeCodec[T9], t10: TypeCodec[T10], t11: TypeCodec[T11], t12: TypeCodec[T12], t13: TypeCodec[T13]): ScalaPreparedStatement13[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, Row] =
+            new ScalaPreparedStatement13[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13)
+
+        /** Prepares a query that will take 14 query parameter
+         *
+         * @return BoundStatement that can be called like a function
+         */
+        def prepare[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14](implicit t1: TypeCodec[T1], t2: TypeCodec[T2], t3: TypeCodec[T3], t4: TypeCodec[T4], t5: TypeCodec[T5], t6: TypeCodec[T6], t7: TypeCodec[T7], t8: TypeCodec[T8], t9: TypeCodec[T9], t10: TypeCodec[T10], t11: TypeCodec[T11], t12: TypeCodec[T12], t13: TypeCodec[T13], t14: TypeCodec[T14]): ScalaPreparedStatement14[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, Row] =
+            new ScalaPreparedStatement14[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14)
+
+        /** Prepares a query that will take 15 query parameter
+         *
+         * @return BoundStatement that can be called like a function
+         */
+        def prepare[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15](implicit t1: TypeCodec[T1], t2: TypeCodec[T2], t3: TypeCodec[T3], t4: TypeCodec[T4], t5: TypeCodec[T5], t6: TypeCodec[T6], t7: TypeCodec[T7], t8: TypeCodec[T8], t9: TypeCodec[T9], t10: TypeCodec[T10], t11: TypeCodec[T11], t12: TypeCodec[T12], t13: TypeCodec[T13], t14: TypeCodec[T14], t15: TypeCodec[T15]): ScalaPreparedStatement15[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, Row] =
+            new ScalaPreparedStatement15[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15)
+
+        /** Prepares a query that will take 16 query parameter
+         *
+         * @return BoundStatement that can be called like a function
+         */
+        def prepare[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16](implicit t1: TypeCodec[T1], t2: TypeCodec[T2], t3: TypeCodec[T3], t4: TypeCodec[T4], t5: TypeCodec[T5], t6: TypeCodec[T6], t7: TypeCodec[T7], t8: TypeCodec[T8], t9: TypeCodec[T9], t10: TypeCodec[T10], t11: TypeCodec[T11], t12: TypeCodec[T12], t13: TypeCodec[T13], t14: TypeCodec[T14], t15: TypeCodec[T15], t16: TypeCodec[T16]): ScalaPreparedStatement16[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, Row] =
+            new ScalaPreparedStatement16[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16)
+
+        /** Prepares a query that will take 17 query parameter
+         *
+         * @return BoundStatement that can be called like a function
+         */
+        def prepare[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17](implicit t1: TypeCodec[T1], t2: TypeCodec[T2], t3: TypeCodec[T3], t4: TypeCodec[T4], t5: TypeCodec[T5], t6: TypeCodec[T6], t7: TypeCodec[T7], t8: TypeCodec[T8], t9: TypeCodec[T9], t10: TypeCodec[T10], t11: TypeCodec[T11], t12: TypeCodec[T12], t13: TypeCodec[T13], t14: TypeCodec[T14], t15: TypeCodec[T15], t16: TypeCodec[T16], t17: TypeCodec[T17]): ScalaPreparedStatement17[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, Row] =
+            new ScalaPreparedStatement17[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17)
+
+        /** Prepares a query that will take 18 query parameter
+         *
+         * @return BoundStatement that can be called like a function
+         */
+        def prepare[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18](implicit t1: TypeCodec[T1], t2: TypeCodec[T2], t3: TypeCodec[T3], t4: TypeCodec[T4], t5: TypeCodec[T5], t6: TypeCodec[T6], t7: TypeCodec[T7], t8: TypeCodec[T8], t9: TypeCodec[T9], t10: TypeCodec[T10], t11: TypeCodec[T11], t12: TypeCodec[T12], t13: TypeCodec[T13], t14: TypeCodec[T14], t15: TypeCodec[T15], t16: TypeCodec[T16], t17: TypeCodec[T17], t18: TypeCodec[T18]): ScalaPreparedStatement18[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, Row] =
+            new ScalaPreparedStatement18[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18)
+
+        /** Prepares a query that will take 19 query parameter
+         *
+         * @return BoundStatement that can be called like a function
+         */
+        def prepare[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19](implicit t1: TypeCodec[T1], t2: TypeCodec[T2], t3: TypeCodec[T3], t4: TypeCodec[T4], t5: TypeCodec[T5], t6: TypeCodec[T6], t7: TypeCodec[T7], t8: TypeCodec[T8], t9: TypeCodec[T9], t10: TypeCodec[T10], t11: TypeCodec[T11], t12: TypeCodec[T12], t13: TypeCodec[T13], t14: TypeCodec[T14], t15: TypeCodec[T15], t16: TypeCodec[T16], t17: TypeCodec[T17], t18: TypeCodec[T18], t19: TypeCodec[T19]): ScalaPreparedStatement19[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, Row] =
+            new ScalaPreparedStatement19[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19)
+
+        /** Prepares a query that will take 20 query parameter
+         *
+         * @return BoundStatement that can be called like a function
+         */
+        def prepare[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20](implicit t1: TypeCodec[T1], t2: TypeCodec[T2], t3: TypeCodec[T3], t4: TypeCodec[T4], t5: TypeCodec[T5], t6: TypeCodec[T6], t7: TypeCodec[T7], t8: TypeCodec[T8], t9: TypeCodec[T9], t10: TypeCodec[T10], t11: TypeCodec[T11], t12: TypeCodec[T12], t13: TypeCodec[T13], t14: TypeCodec[T14], t15: TypeCodec[T15], t16: TypeCodec[T16], t17: TypeCodec[T17], t18: TypeCodec[T18], t19: TypeCodec[T19], t20: TypeCodec[T20]): ScalaPreparedStatement20[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, Row] =
+            new ScalaPreparedStatement20[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20)
+
+        /** Prepares a query that will take 21 query parameter
+         *
+         * @return BoundStatement that can be called like a function
+         */
+        def prepare[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21](implicit t1: TypeCodec[T1], t2: TypeCodec[T2], t3: TypeCodec[T3], t4: TypeCodec[T4], t5: TypeCodec[T5], t6: TypeCodec[T6], t7: TypeCodec[T7], t8: TypeCodec[T8], t9: TypeCodec[T9], t10: TypeCodec[T10], t11: TypeCodec[T11], t12: TypeCodec[T12], t13: TypeCodec[T13], t14: TypeCodec[T14], t15: TypeCodec[T15], t16: TypeCodec[T16], t17: TypeCodec[T17], t18: TypeCodec[T18], t19: TypeCodec[T19], t20: TypeCodec[T20], t21: TypeCodec[T21]): ScalaPreparedStatement21[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, Row] =
+            new ScalaPreparedStatement21[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21)
+
+        /** Prepares a query that will take 22 query parameter
+         *
+         * @return BoundStatement that can be called like a function
+         */
+        def prepare[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22](implicit t1: TypeCodec[T1], t2: TypeCodec[T2], t3: TypeCodec[T3], t4: TypeCodec[T4], t5: TypeCodec[T5], t6: TypeCodec[T6], t7: TypeCodec[T7], t8: TypeCodec[T8], t9: TypeCodec[T9], t10: TypeCodec[T10], t11: TypeCodec[T11], t12: TypeCodec[T12], t13: TypeCodec[T13], t14: TypeCodec[T14], t15: TypeCodec[T15], t16: TypeCodec[T16], t17: TypeCodec[T17], t18: TypeCodec[T18], t19: TypeCodec[T19], t20: TypeCodec[T20], t21: TypeCodec[T21], t22: TypeCodec[T22]): ScalaPreparedStatement22[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, Row] =
+            new ScalaPreparedStatement22[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, Row](session.prepare(query), RowMapper.identity, StatementOptions.default, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21, t22)
+
+        // format: on
+    end SyncCQLQuery
 
     //     def prepareUnit: ScalaPreparedStatement[Unit, Row] = ???
 
     //     def prepare[T1]: ScalaPreparedStatement[T1, Row] = ???
 
     //     def prepare[T1, T2]: ScalaPreparedStatement1[(T1, T2), Row] = ???
-        
+
     //     def prepare[T1, T2, T3]: ScalaPreparedStatement2[(T1, T2, T3), Row] = ???
-    
+
     // abstract class ScalaPreparedStatement1[A, B]
     // abstract class ScalaPreparedStatement2[A, B]
 
