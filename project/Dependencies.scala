05efab985be455bde4bfbf9fd61bab511bf2be42
Scala Steward <scala_steward@virtuslab.com>
2024-10-16 18:43:39 +0000

    Update mockito-core to 5.14.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val slf4j         = "2.0.16"
 
     // Test Dependencies
-    val mockito       = "5.14.0"
+    val mockito       = "5.14.2"
     val scalaCheck    = "1.18.1"
     val scalaTest     = "3.2.19"
     val scalaTestPlus = "3.2.18.0"
