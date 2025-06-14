c49c2b2fc66a45ee12ba25e82ef2ec7536bcab9c
Scala Steward <scala_steward@virtuslab.com>
2025-06-14 00:38:24 +0000

    Update pekko-stream, pekko-testkit to 1.1.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
 
     // Pekko Dependencies
     val pekkoConnector = "1.0.2"
-    val pekkoTestKit   = "1.1.3"
+    val pekkoTestKit   = "1.1.4"
 
     val zio               = "2.1.19"
     val zioStreamsInterop = "2.0.2"
