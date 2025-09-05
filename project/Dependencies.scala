ff433b1a2e6e67301873eb3d50b58df9a4a50b5c
Scala Steward <scala_steward@virtuslab.com>
2025-09-05 16:51:15 +0000

    Update zio, zio-streams, zio-test, ... to 2.1.21

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
     val pekkoConnector = "1.0.2"
     val pekkoTestKit   = "1.2.1"
 
-    val zio               = "2.1.20"
+    val zio               = "2.1.21"
     val zioStreamsInterop = "2.0.2"
   }
 
